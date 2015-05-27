2951d5420ae5ec29679da77f33418eda9c51841e
Toby Alden & Courtney Phillps <student@epicodus.com>
2015-05-27 09:42:21 -0700

    Add 'ay' to the end of words beginning with a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-// var pig_latin = function(phrase) {
-//
-// }
+var pig_latin = function(phrase) {
+  if(is_vowel(phrase.charAt(0))) {
+    phrase = phrase.concat("ay");
+  }
+  return phrase;
+};
 
 var is_vowel = function(letter) {
-  if(letter.charAt(0).match(/[aeiou]/) != null) {
+  if(letter.match(/[aeiou]/) != null) {
     return true;
   }
   else {
     return false;
   }
-}
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,11 @@
+describe('pig_latin', function() {
+
+  it("adds 'ay' to the end of words that start with a vowel", function() {
+    expect(pig_latin('abacus')).to.equal("abacusay");
+  });
+
+});
+
 describe('is_vowel', function() {
 
   it("detects if a letter is a vowel", function() {
@@ -7,6 +15,5 @@ describe('is_vowel', function() {
   it("detects if a letter is NOT a vowel", function() {
     expect(is_vowel('b')).to.equal(false);
   });
-
 
 });
